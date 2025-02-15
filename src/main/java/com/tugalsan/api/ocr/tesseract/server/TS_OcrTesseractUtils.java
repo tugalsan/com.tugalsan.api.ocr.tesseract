cfa340e38acac5154e0209ab0b3d50121fc19ca3
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-02-15 14:40:36 +0300

    Update TS_OcrTesseractUtils.java

--- src/main/java/com/tugalsan/api/ocr/tesseract/server/TS_OcrTesseractUtils.java
+++ src/main/java/com/tugalsan/api/ocr/tesseract/server/TS_OcrTesseractUtils.java
@@ -4,6 +4,7 @@ import com.tugalsan.api.log.server.TS_Log;
 
 public class TS_OcrTesseractUtils {
 
+    //INSTALL https://github.com/UB-Mannheim/tesseract/wiki
     final private static TS_Log d = TS_Log.of(TS_OcrTesseractUtils.class);
 
 }
